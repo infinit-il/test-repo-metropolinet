596914af93c8327daa8e2eead172c1c7a1407469
Apex3 <Apex3@Metropolinet.Local>
2023-08-27 16:07:33 +0300

    3chocoPacks

---
+++ branch84/tools/branch84code.ps1
@@ -0,0 +1,12 @@
+
+
+$username = "hr2"
+$password = "hr2"
+$db = "172.30.200.42"     
+$port = "1521"
+$service_name = "orc1"
+$pathURL = "D:\choco-dependence\rm_branch_codes_table.xml"
+
+
+"lb update -changelog-file $pathUR\rm_branch_codes_table.xml" | & "sql.exe" '-S' "${username}/${password}@${db}:${port}/${service_name}"
+
